dcb558cd68989be58c87ddac60ec759a152b67f3
williamcai <williamcai663@163.com>
2019-06-12 23:39:54 +0800

    test bak

--- src/main/java/com/alibaba/learning/endpoint/TestBuilder.java
+++ src/main/java/com/alibaba/learning/endpoint/TestBuilder.java
@@ -4,7 +4,8 @@ import com.alibaba.learning.bean.User;
 
 public class TestBuilder {
 
-    public static void main(String[] args){
-      User user=new User.Builder(10L).name("jackcai").className("No1").gender("man").build();
+    public static void main(String[] args) {
+        User user = new User.Builder(10L).name("jackcai").className("No1").gender("man").build();
+
     }
 }
